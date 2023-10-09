88afc725cceaf8b463874332238e4e5d02297b2f
Itohan1 <momoduitohan5@gmail.com>
2023-10-09 01:48:51 -0700

    yiou

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,12 @@
  *
  * Return : Always (0)
  */
+
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 26; i * *)
-	{
-		putchar(alp[i]);
-	}
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
