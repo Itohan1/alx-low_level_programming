31f27bee29e31699f2c9c7b41c9e4618015022e3
Itohan1 <momoduitohan5@gmail.com>
2023-10-11 04:41:17 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/**
+ * print_alphabet - Functions to print lowercase alphabets
+ *
+ * Return: return 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
