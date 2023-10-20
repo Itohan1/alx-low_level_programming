4f0d4b724e2a70810dbc0fa712165181bf7b641d
Itohan1 <momoduitohan5@gmail.com>
2023-10-20 07:06:18 -0700

    uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string parameter
+ * Return: str
+ *
+ */
+
+char *string_toupper(char *str)
+{
+	int j;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		if (str[j] > 'a' && str[j] <= 'z')
+			str[j] = str[j] - 32;
+		j++;
+	}
+	return (str);
+}
