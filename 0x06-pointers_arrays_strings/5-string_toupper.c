5c7bf634dc3e838180e89f03a702a91b68d42daa
Itohan1 <momoduitohan5@gmail.com>
2023-10-20 07:11:47 -0700

    upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,7 +14,7 @@ char *string_toupper(char *str)
 	j = 0;
 	while (str[j] != '\0')
 	{
-		if (str[j] > 'a' && str[j] <= 'z')
+		if (str[j] >= 'a' && str[j] <= 'z')
 			str[j] = str[j] - 32;
 		j++;
 	}
