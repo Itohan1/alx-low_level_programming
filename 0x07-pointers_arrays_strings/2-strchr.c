6461746ad83fe45d73673dffbdf1215796e0f39e
Itohan1 <momoduitohan5@gmail.com>
2023-10-23 08:33:43 -0700

    Null

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: first parameter
+ * @c: second parameter
+ * Return: c
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int j;
+
+	for (j = 0 ; s[j] != '\0' ; j++)
+	{
+		if (s[0] == c)
+			return (s + j);
+	}
+	return (NULL);
+}
