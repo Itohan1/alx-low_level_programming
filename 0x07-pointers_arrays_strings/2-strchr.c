4ba08dd0b1c40a6af59381173b9e3521eb03703b
Itohan1 <momoduitohan5@gmail.com>
2023-10-23 08:38:27 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,9 +12,9 @@ char *_strchr(char *s, char c)
 {
 	int j;
 
-	for (j = 0 ; s[j] != '\0' ; j++)
+	for (j = 0 ; s[j] >= '\0' ; j++)
 	{
-		if (s[0] == c)
+		if (s[j] == c)
 			return (s + j);
 	}
 	return (NULL);
