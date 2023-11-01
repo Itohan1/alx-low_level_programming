469d02bf2fac011728ab52c17162807bb3f7a9d7
Itohan1 <momoduitohan5@gmail.com>
2023-11-01 05:30:38 -0700

    Natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: first number
+ * Return: 0
+ *
+ */
+
+void print_to_98(int n)
+{
+	int i = n;
+
+	if (i >= n && i <= 98)
+	{
+		for (i = n ; i >= n && i <= 98 ; i++)
+		{
+			if (i == n)
+			{
+				printf("%d,", i);
+			}
+			else if (i > n && i < 98)
+			{
+				printf(" %d,", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+		printf("\n");
+	}
+	else if (i <= n && i >= 98)
+	{
+		for (i = n ; i <= n && i >= 98 ; i--)
+		{
+			if (i == n)
+			{
+				printf("%d,", i);
+			}
+			else if (i > 98 && i < n)
+			{
+				printf(" %d,", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+		printf("\n");
+	}
+}
