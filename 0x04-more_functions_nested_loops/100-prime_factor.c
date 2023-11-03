bdff5d047a279f996309502f8583f4964641c570
Itohan1 <momoduitohan5@gmail.com>
2023-11-03 07:10:18 -0700

    prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - finds the prime numbers
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	long n = 612852475143;
+	long j;
+
+	while (j != n)
+	{
+		if (n % j == 0)
+			n = n / j;
+		else
+			j++;
+	}
+	printf("%ld\n", j);
+	return (0);
+}
