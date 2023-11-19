abab2bb83f90869ba36b1b4183ccf8504412c694
Itohan1 <momoduitohan5@gmail.com>
2023-11-19 07:30:30 -0800

    remove

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-int main(int __attribute__((unused)) argc, char *argv[]);
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
