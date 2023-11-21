2a7e4c88675e86f4aa8fc1c2857e07cf7f759d66
Itohan1 <momoduitohan5@gmail.com>
2023-11-21 04:06:27 -0800

    correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *tem = *head;
 	listint_t *node = NULL;
-	unsigned int q;
+	unsigned int q = 0;
 
 	if (*head == NULL)
 		return (-1);
