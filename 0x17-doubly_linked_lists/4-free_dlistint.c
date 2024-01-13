34946e4dd39302bd8a4af9691803af24e38755db
Itohan1 <momoduitohan5@gmail.com>
2024-01-13 12:16:21 -0800

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ * @head: parameter
+ * Return: success
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
