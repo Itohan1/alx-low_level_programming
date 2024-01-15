900ffdd4f8d184d59bd74cabf065eca2a5f26923
Itohan1 <momoduitohan5@gmail.com>
2024-01-15 11:32:07 -0800

    all

---
+++ 0x18-dynamic_libraries/every.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * add - function for multiplication
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
+
+/**
+ * sub - function for subtraction
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0
+ */
+
+int sub(int a, int b)
+{
+	return(a - b);
+}
+
+/**
+ * division - function for division
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0
+ */
+
+int div(int a, int b)
+{
+	return(a / b);
+}
