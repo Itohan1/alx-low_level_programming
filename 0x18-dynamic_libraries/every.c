561725ca1289a8acac73110c435fd136d1501f17
Itohan1 <momoduitohan5@gmail.com>
2024-01-15 11:44:22 -0800

    all

--- 0x18-dynamic_libraries/every.c
+++ 0x18-dynamic_libraries/every.c
@@ -35,3 +35,27 @@ int div(int a, int b)
 {
 	return(a / b);
 }
+
+/**
+ * add - function for addition
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
+
+/**
+ * mod - function for modulus
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0
+ */
+
+int mod(int a, int b)
+{
+	return(a % b);
+}
