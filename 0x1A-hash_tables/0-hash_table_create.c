40fcc47796db1ec025ee9dfa74c651572a927387
Itohan1 <momoduitohan5@gmail.com>
2024-02-01 06:31:39 -0800

    create hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: string parameter
+ * Return: NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *t;
+	unsigned long int i;
+
+	t = malloc(sizeof(hash_table_t));
+	if (t == NULL)
+		return (NULL);
+	t->size = size;
+	t->array = malloc(sizeof(hash_node_t *) * size);
+	if (t->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		t->array[i] = NULL;
+
+	return (t);
+}
