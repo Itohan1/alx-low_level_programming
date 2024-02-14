5662dea6a4e76b483bfd7af4f472e02536d30f2a
Itohan1 <momoduitohan5@gmail.com>
2024-02-14 04:13:48 -0800

    4-hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,9 +1,27 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 
 /**
+ * hash_table_get - retrieves a value
+ * @ht: first parameter
+ * @key: second parameter
+ * Return: 0
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
+	hash_node_t *nod;
+	unsigned long int id;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	id = key_index((const unsigned char *)key, ht->size);
+	if (id >= ht->size)
+		return (NULL);
+
+	nod = ht->array[id];
+	while (nod && strcmp(nod->key, key) != 0)
+		nod = nod->next;
+
+	return ((nod == NULL) ? NULL : nod->value);
+}
