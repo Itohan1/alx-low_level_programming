c8967a9d4342cecc956ed92f78bac998dcca2606
Itohan <momoduitohan5@gmail.com>
2024-05-07 21:28:09 +0100

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search function
+ * @array: array parameter
+ * @size: size parameter
+ * @value: value parameter
+ * Return: 0
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0 ; i < size ; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+	return (-1);
+}
