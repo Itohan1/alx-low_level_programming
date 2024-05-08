8b8b14250503c64e37b4df191f03ee87476fb59a
Itohan <momoduitohan5@gmail.com>
2024-05-08 20:51:40 +0100

    these files

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,19 +9,21 @@ int jump_search(int *array, size_t size, int value)
 	int j = size - 1;
 	int low = 0;
 	int pres;
-	int i;
-
 	if (array == NULL)
 		return (-1);
 
-	while (low <= jumps && jumps <= j)
+	while (low <= j && jumps <= j)
 	{
 		pres = low;
 		printf("Value checked array [%d] = [%d]\n", pres, pres);
 		if (array[pres] == value)
-			return (pres);
-		if (array[pres] < value && array[jumps] > value)
 		{
+			return (pres);
+		}
+		if ((array[pres] < value) && (array[jumps] > value || array[jumps] > j))
+		{
+			int i;
+
 			printf("Value found between indexes [%d] and [%d]\n", pres, jumps);
 			for (i = pres; i <= jumps; i++)
 			{
@@ -31,11 +33,9 @@ int jump_search(int *array, size_t size, int value)
 					return (value);
 				}
 			}
-
 		}
-		printf("\n");
 		low = jumps;
-		jumps += sqrt(size);
+		jumps = low + sqrt(size);
 	}
 	return (-1);
 }
