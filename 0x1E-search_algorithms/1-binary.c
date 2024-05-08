487dacabb786c7b74966b6eaa1b299bfbd007041
Itohan <momoduitohan5@gmail.com>
2024-05-08 12:15:21 +0100

    binary search tree

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - binary search algorithmn
+ * @array: array parameter
+ * @size: size parameter
+ * @value: value parameter
+ * Return: -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int high = size - 1;
+	int mid;
+
+	if (array == NULL)
+		return (-1);
+
+	printf("Searching in array: ");
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (mid = low; mid <= high; mid++)
+		{
+			printf("%d", array[mid]);
+			if (mid < high)
+				printf(", ");
+		}
+		printf("\n");
+
+		mid = (low + high) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+
+	}
+	return (-1);
+}
